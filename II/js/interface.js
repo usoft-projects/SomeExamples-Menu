03742591193359239fc89a00dd856eca7dd08f0f
Uğur Şenaslan <senaslanugur@gmail.com>
2022-08-02 22:58:41 +0300

    main

--- II/js/interface.js
+++ II/js/interface.js
@@ -12,15 +12,24 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 var ref = firebase.database().ref();
-
 ref.on("value", function(snapshot) {
     var test = snapshot.val()
     var keys = Object.keys(test);
     test = (JSON.stringify(test))
     test = (JSON.parse(test))
 
+    var categories = document.getElementById("categories")
+    categories.innerHTML = ""
+    var $text = $('#categories');
+    for(var i=0;i<keys.length;i++){
+        $text.append('<div class="single-blog relative"><div class="section-bg color" style="border-radius: 44px ;">'+
+        '<div class="section-bg" style="background-image: url(images/blog/blog-1.jpg); " ></div> </div><div class="post-date">Bolu Mangal Keyfi - Kategoriler</div>'+
+        '<div class="space-10"></div><a href="#'+keys[i]+'"> <h3>'+keys[i]+'</h3></a></div>');
+    }
+    
     var products = document.getElementById("products")
     for(var i=0;i<keys.length;i++){
+        products.innerHTML += '<div id="'+keys[i]+'">'
         for(var k=0;k<test[keys[i]].length;k++){
             products.innerHTML += '<div class="col-xs-12 col-sm-6"><div class="single-menu"><div class="media" style="border-radius: 44px ; box-shadow: -9px 9px 0px -2px rgba(253,93,93,0.38);"> <div class="media-left">'+
             ' <div class="menu-img" style="background-image: url(&apos;'+test[keys[i]][k].image+'&apos;)"><img src="images/menu/ovr.png" alt="">'+
@@ -28,12 +37,9 @@ ref.on("value", function(snapshot) {
             ' </a></td><td><div class="menu-rate">'+test[keys[i]][k].price+' TL </div></td></tr></table><p>'+test[keys[i]][k].details+'</p>'+
             '</div></div></div><div class="space-30"></div></div>'
         }
-        
+        products.innerHTML += '</div>'
     }
 
 }, function (error) {
     console.log("Error: " + error.code);
 });
-
-
-
